1915e8a3d41a0c7abfba0081460d70ce21e8d7bb
msr <debashis.education@gmail.com>
2024-07-21 15:52:05 +0530

    new commit

---
+++ Introduction_31_March_2024/src/zenith/FirstSeleniumTest.java
@@ -0,0 +1,53 @@
+package zenith;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstSeleniumTest {
+
+	static String browser;
+	static WebDriver driver;
+	public static void main(String[] args) {
+		setBrowser();
+		setBrowserConfig();
+		runTest();
+
+		
+	}
+
+	// set Browser
+	// set Browser config
+	//run test
+
+	public static void setBrowser() {
+		browser="Firefox";
+	}
+
+	public static void setBrowserConfig() {
+		
+//		String projectLocation = System.getProperty("user.dir");
+		if(browser.contains("Firefox")) {
+	
+		driver = new FirefoxDriver();
+		}
+		if(browser.contains("Chrome")) {
+		
+		driver = new ChromeDriver();
+		}
+		
+	}
+
+	public static void runTest() {
+		
+//		driver.manage().window().maximize();
+				driver.get("http://111.93.227.212/zmp/login.do");
+		//		driver.get("http://172.30.3.13/zmp/login.do");
+//		driver.get("https://www.google.com");
+		
+		driver.quit();
+	
+
+	}
+
+}
